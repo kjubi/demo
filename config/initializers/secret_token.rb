9885a18d906d30040b1d6cff365bddd063bcb500
kjubi <estarshono@gmail.com>
2011-12-01 02:06:37 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoAppScaffolfds::Application.config.secret_token = '2cba5fb662f0e61a3db65836e233e52a6be2d033dbd272621d0b5c75efd2ddadc970120a937d88394b52be0dfdf91e6c199aa5baeb543559982d8d0e19b92a24'
